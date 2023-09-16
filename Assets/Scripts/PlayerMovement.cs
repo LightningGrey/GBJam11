7dda24f8e13774cc1ff9243896c8d0a718fcf0af
LightningGrey <hunt3rchu@gmail.com>
2023-09-16 01:40:49 -0400

    set up base scene and base movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GBTemplate;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private GBConsoleController gb;
+	private Camera cam;
+	public float speed = 0.5f;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		gb = GBConsoleController.GetInstance();
+		cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log(cam.WorldToScreenPoint(transform.position));
+		if (gb.Input.Up)
+		{
+			transform.position += new Vector3(0f, 1f) * speed * Time.deltaTime;
+		}
+		if (gb.Input.Down)
+		{
+			transform.position -= new Vector3(0f, 1f) * speed * Time.deltaTime;
+		}
+		if (gb.Input.Left)
+		{
+			transform.position -= new Vector3(1f, 0f) * speed * Time.deltaTime;
+		}
+		if (gb.Input.Right)
+		{
+			transform.position += new Vector3(1f, 0f) * speed * Time.deltaTime;
+		}
+	}
+}
